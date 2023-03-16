236a14afc90e6c1f7f6a289cc5b219a9d96d4d94
Julien Salleyron <julien.salleyron@gmail.com>
2023-03-16 10:04:56 +0100

    Adds Yaegi

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
   in {
     packages.x86_64-linux.prm-bin = import ./prm-bin.nix { inherit pkgs; };
     packages.x86_64-linux.prm = pkgs.callPackage ./prm.nix {}; 
+    packages.x86_64-linux.yaegi = pkgs.callPackage ./yaegi.nix {}; 
     
   };
 }
---
+++ yaegi.nix
@@ -0,0 +1,32 @@
+
+{ lib, fetchFromGitHub, buildGoModule, installShellFiles }:
+
+buildGoModule rec {
+  pname = "yaegi";
+  version = "0.15.0";
+
+  src = fetchFromGitHub {
+    owner = "traefik";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-++WA6Xdo9YuMQgCL/c485LgKeV4XeodVZBBYCBsmh+M=";
+  };
+
+  vendorHash =null;
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  doCheck = false;
+
+  ldflags = [
+    "-s" "-w"
+    " -X main.version=${version}"
+  ];
+
+  meta = with lib; {
+    description = "Yaegi is Another Elegant Go Interpreter";
+    license = licenses.asl20;
+    homepage = "https://pkg.go.dev/github.com/traefik/yaegi";
+  };
+
+}
