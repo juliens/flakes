9fd317769229c095e81db95721fdabd0ca671a1d
Julien Salleyron <julien.salleyron@gmail.com>
2023-06-22 23:05:37 +0200

    Adds go1.21rc2

--- flake.nix
+++ flake.nix
@@ -15,6 +15,24 @@
       packages.prm-bin = import ./prm-bin.nix { inherit pkgs; };
       packages.prm = pkgs.callPackage ./prm.nix {}; 
       packages.yaegi = pkgs.callPackage ./yaegi.nix {}; 
+      packages.go_1_21 = pkgs.go.overrideAttrs (finalAttrs: rec {
+        version = "1.21rc2";
+        src = builtins.fetchurl {
+          url = "https://go.dev/dl/go${version}.src.tar.gz";
+          sha256 ="0vh72fv443bb7hdmrnbpwxrqbpcvyclrid6jwrxjs7yr4b76snci";
+        };
+        patches = nixpkgs.lib.lists.remove (nixpkgs.lib.lists.last finalAttrs.patches) finalAttrs.patches;
+      });
+
+#     packages.traefikImage = pkgs.dockerTools.buildImage {
+#       name = "traefik";
+#       tag = "nix";
+#       copyToRoot=[ pkgs.traefik ];
+#       config = {
+#         Cmd = [ "${pkgs.traefik}/bin/traefik" ];
+#       };
+
+#     };
     }
     );
     
--- prm-bin.nix
+++ prm-bin.nix
@@ -3,8 +3,8 @@ pkgs.stdenv.mkDerivation rec {
   name = "prm";
   version="v3.5.0";
   src = pkgs.fetchzip {
-    url = "https://github.com/ldez/prm/releases/download/v3.5.0/prm_${version}_linux_amd64.tar.gz";
-    sha256="A1cuCtwDMTqYi1kqctOD7JC8zCMnpeRbN1bm/fbSX1Q=";
+    url = "https://github.com/ldez/prm/releases/download/v3.5.0/prm_v3.5.0_linux_amd64.tar.gz";
+    sha256="0m2zsbvgvrjn6xdy99974g6br47chg9p4ajrifc3lc83vh52wmq3";
     stripRoot=false;
   };
   phases = ["installPhase" "patchPhase"];
