0b1641102936fe419ea27ad40024388140c60c88
Julien Salleyron <julien.salleyron@gmail.com>
2023-08-18 14:26:25 +0200

    Update go1.21

--- flake.nix
+++ flake.nix
@@ -17,10 +17,10 @@
       packages.mocktail = pkgs.callPackage ./mocktail.nix {}; 
       packages.yaegi = pkgs.callPackage ./yaegi.nix {}; 
       packages.go_1_21 = pkgs.go.overrideAttrs (finalAttrs: rec {
-        version = "1.21rc2";
+        version = "1.21.0";
         src = builtins.fetchurl {
           url = "https://go.dev/dl/go${version}.src.tar.gz";
-          sha256 ="0vh72fv443bb7hdmrnbpwxrqbpcvyclrid6jwrxjs7yr4b76snci";
+          sha256 ="16in2khjq7b0anvvangc29ph0iyjlhvyyy6k39axv0jnx3nld3c1";
         };
         patches = nixpkgs.lib.lists.remove (nixpkgs.lib.lists.last finalAttrs.patches) finalAttrs.patches;
       });
