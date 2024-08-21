470456e6d7b64b1099eab28df345731e745a6419
Julien Salleyron <julien.salleyron@gmail.com>
2024-08-21 11:27:47 +0200

    Adds go with commit which merge wasmexport

--- flake.nix
+++ flake.nix
@@ -17,6 +17,23 @@
       packages.tparse = pkgs.callPackage ./tparse.nix {}; 
       packages.mocktail = pkgs.callPackage ./mocktail.nix {}; 
       packages.yaegi = pkgs.callPackage ./yaegi.nix {}; 
+      packages.go_commit = pkgs.go_1_23.overrideAttrs (finalAttrs: rec {
+        GOROOT_BOOTSTRAP="${pkgs.go_1_23}/share/go";
+        version = "f38d42f2c4c6ad0d7cbdad5e1417cac3be2a5dcb";
+        buildInputs = [ pkgs.git ] ++ finalAttrs.buildInputs;
+        src = pkgs.fetchFromGitHub {
+          owner = "golang";
+          repo = "go";
+          rev = "f38d42f2c4c6ad0d7cbdad5e1417cac3be2a5dcb";
+          sha256 = "JHx/1foo1CEuMO0T4+gmSD6OO02Q5A1tBDPy2MULd5A=";
+      
+        };
+        #patches = nixpkgs.lib.lists.remove (nixpkgs.lib.lists.last finalAttrs.patches) finalAttrs.patches;
+        buildPhase = ''
+          export PATH="$PATH:${pkgs.git}/bin"
+          echo "f38d42f2c4c6ad0d7cbdad5e1417cac3be2a5dcb" > VERSION
+          '' + finalAttrs.buildPhase;
+      });
     }
     );
     
